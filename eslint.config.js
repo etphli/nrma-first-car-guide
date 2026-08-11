7b825e6b618a80806f4992bf36216a042876a7d9
Your Name <likht.ethan@emanuelschool.nsw.edu.au>
2026-08-11 11:53:52 +1000

    Build NRMA first car guide website

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+      },
+    },
+    rules: {},
+  },
+];
